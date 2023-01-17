828cb31f14d779b987fa2f692c90f0890f8eae0f
amaraoussama94 <wassa36@yahoo.fr>
2023-01-17 07:19:52 +0100

    caractere

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() { //fn  to say this is a function
     println!(" {} ", c);// using  default  format
     println!(" {:08.3} ", c); //3 digit of precision, totale of 8 caractere 
     print!(" {0:08.3}  not  {1}", c,first_var); //ln for  new line every var  has  position starting  from 0 using this position you can prin t it many tima in ssame line 
-
+    let _first_char = 'a';//create caractere 
+    let  finger = '\u{261D}';// use  symbole
+    println!("{}",finger);
 
 }
